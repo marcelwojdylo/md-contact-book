24a2c10bbea0eb975b9d688464259d7155973051
Marcel Wojdylo <marcelwojdylo@gmail.com>
2019-06-04 13:48:11 +0200

    1. Created non-instantiable utility class ContactDataFormatter for keeping fields of class Contact uniform and remove non-digit characters or non-letter characters as needed, 2. Modified data types for fields in class Contact, 3. Created method in class ContactBook for sorting contacts by last name initial and method for printing the resulting list. NEXT STEP:

--- system/ContactBookApp.java
+++ system/ContactBookApp.java
@@ -1,11 +1,35 @@
+
 public class ContactBookApp {
     public static void main(String[] arg) {
         System.out.println("\n");
         ContactBook contactBook = new ContactBook();
-        contactBook.printContactsWithLastNameInitial('C');
-        contactBook.printContactsWithLastNameInitial('U');
+        // contactBook.printContactBook();
+        contactBook.printContactsSortedByLastNameInitial();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class ContactBook {
     private Contact[] contacts;
@@ -37,17 +61,40 @@ class ContactBook {
     }
     
     // METHODS FOR SORTING CONTACTS 
+
+    public Contact[] sortContactsByLastNameInitial() {
+        Contact[] result = new Contact[Contact.getNumberOfContacts()];
+        int indexCounter = 0;
+
+        for (int i = 0; i < Constants.LETTERS.length(); i++) {
+            for (int j = 0; j < Contact.getNumberOfContacts(); j++) {
+                if (contacts[j] == null) {
+                    continue;
+                }
+
+                if (contacts[j].lastName.charAt(0) == Constants.LETTERS.charAt(i)) {
+                    result[indexCounter] = contacts[j];
+                    indexCounter++;
+                }
+            }
+        }
+
+        return result;
+    }
     
-    public void printContactsWithLastNameInitial(char initial) {
+    public Contact[] getContactsWithLastNameInitial(char initial) {
+        Contact[] result = new Contact[Contact.getNumberOfContacts()];
         for (int i = 0; i <= Contact.getNumberOfContacts(); i ++) {
             if (contacts[i] == null) {
                 continue;
             }
             if (contacts[i].lastName.charAt(0) == initial) {
-                System.out.println(contacts[i]);
+                result[i] = contacts[i];
             }
 
         }
+
+        return result;
     }
     
     // METHODS FOR PRINTING
@@ -60,6 +107,20 @@ class ContactBook {
             System.out.println(this.contacts[i]);
         } 
     }   
+
+    public void printContactsSortedByLastNameInitial() {
+        for (Contact c : sortContactsByLastNameInitial()) {
+            if (c == null) {continue;}
+            System.out.println(c);
+        }
+    }
+
+    public void printContactsWithLastNameInitial(char initial) {
+        for (Contact c : getContactsWithLastNameInitial(initial)) {
+            if (c == null) {continue;}
+            System.out.println(c);
+        }
+    }
     
     public void printContact(int contactID) {
         System.out.println(this.getContacts()[contactID]);
@@ -85,14 +146,14 @@ class ContactBook {
             contactArray[i] = new Contact.Builder()
                 .lastName(this.genericSurnames()[i])
                 .firstName("Generyk")
-                .phoneNumber("666-666-666")
-                .dateOfBirth("13/12/1990")
+                .phoneNumber("666-666-666oijdsfknadsf")
+                .dateOfBirth(13, 12, 1990)
                 .addressStreet("Rajska")
                 .addressHouse("2")
-                .addressFlat("96")
+                .addressFlat(1293)
                 .addressPostcode("02-972")
-                .addressCity("Warszawa")
-                .addressCountry("Polska")
+                .addressCity("warszawa")
+                .addressCountry("polska")
                 .email("generyk@generyk.gn")
                 .build();
             i++;    
@@ -108,7 +169,7 @@ class ContactBook {
         stringArray[0] = "Abrowski";
         stringArray[1] = "Boniewski";
         stringArray[2] = "Czarnobylski";
-        stringArray[3] = "Czekowski";
+        stringArray[3] = "czeKOws00::::::ki";
         stringArray[4] = "Frajowski";
         stringArray[5] = "Szakajowski";
         stringArray[6] = "Bombalakowski";
@@ -119,6 +180,24 @@ class ContactBook {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -140,7 +219,10 @@ class Contact {
     }
     public final String firstName;
     public final String lastName;
-    public final String dateOfBirth;
+    public final int[] dateOfBirth;
+    private static final int DAY_OF_BIRTH_INDEX = 0;
+    private static final int MONTH_OF_BIRTH_INDEX = 1;
+    private static final int YEAR_OF_BIRTH_INDEX = 2;
     public final String addressStreet;
     public final String addressHouse;
     public final String addressFlat;
@@ -150,6 +232,8 @@ class Contact {
     public final String phoneNumber;
     public final String email;
 
+
+
     public static class Builder {
         //Required parameters
         private final int contactID;
@@ -157,7 +241,7 @@ class Contact {
         //Optional parameters and their defaults
         private String firstName = "";
         private String lastName = "";
-        private String dateOfBirth = "";
+        private int[] dateOfBirth = new int[3];
         private String addressStreet = "";
         private String addressHouse = "";
         private String addressFlat = "";
@@ -173,22 +257,24 @@ class Contact {
         }
 
         public Builder firstName (String value) {
-            firstName = value;
+            firstName = ContactDataFormatter.formatProperName(value);
             return this;
         }
 
         public Builder lastName (String value) {
-            lastName = value;
+            lastName = ContactDataFormatter.formatProperName(value);
             return this;
         }
 
-        public Builder dateOfBirth (String value) {
-            dateOfBirth = value;
+        public Builder dateOfBirth (int day, int month, int year) {
+            dateOfBirth[DAY_OF_BIRTH_INDEX] = day;
+            dateOfBirth[MONTH_OF_BIRTH_INDEX] = month;
+            dateOfBirth[YEAR_OF_BIRTH_INDEX] = year;
             return this;
         }
 
         public Builder addressStreet (String value) {
-            addressStreet = value;
+            addressStreet = ContactDataFormatter.formatProperName(value);
             return this;
         }
 
@@ -197,8 +283,20 @@ class Contact {
             return this;
         }
 
+        public Builder addressHouse (int value) {
+            String string = Integer.toString(value);
+            addressHouse = string;
+            return this;
+        }
+
         public Builder addressFlat (String value) {
             addressFlat = value;
+            return this;
+        }
+
+        public Builder addressFlat (int value) {
+            String string = Integer.toString(value);
+            addressFlat = string;
             return this;
         }
 
@@ -208,19 +306,26 @@ class Contact {
         }
 
         public Builder addressCity (String value) {
-            addressCity = value;
+            addressCity = ContactDataFormatter.formatProperName(value);
             return this;
         }
 
         public Builder addressCountry (String value) {
-            addressCountry = value;
+            addressCountry = ContactDataFormatter.formatProperName(value);
             return this;
         }
 
         public Builder phoneNumber (String value) {
-            phoneNumber = value;
+            phoneNumber = ContactDataFormatter.formatPhoneNumber(value);
             return this;
         }
+
+        public Builder phoneNumber (int value) {
+            String string = Integer.toString(value);
+            phoneNumber = ContactDataFormatter.formatPhoneNumber(string);
+            return this;
+        }
+        
 
         public Builder email (String value) {
             email = value;
@@ -247,15 +352,157 @@ class Contact {
         email = builder.email;
     }
 
+    // TODO: CONVERT TO LIST<STRING> TYPE OBJECT
+
+    private static boolean contains(final int[] array, final int v) {
+        boolean result = false;
+        for (int i : array) {
+            if (i == v) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+
+    //METHODS FOR PRINTING
+
     public String toString() {
+        String result = "";
+
+
         return 
             "Contact ID: " + contactID + ",\n" +
             "First Name: " + firstName + ",\n" +
             "Last Name: " + lastName + ",\n" +
-            "Date of Birth: " + dateOfBirth + ",\n" +
+            "Date of Birth: " + dateOfBirth[DAY_OF_BIRTH_INDEX] + "/" + dateOfBirth[MONTH_OF_BIRTH_INDEX] + "/" + dateOfBirth[YEAR_OF_BIRTH_INDEX] + ",\n" +
             "Address:" + addressStreet + " " + addressHouse + "/" + addressFlat + ",\n" +
             "         " + addressPostcode + " " + addressCity + ", " + addressCountry + ",\n" +
             "Phone number: " + phoneNumber + ",\n" +
             "E-mail: " + email + "\n";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Constants {
+    public final static String DIGITS = "1234567890";
+    public final static String LETTERS = "aąbcćdeęfghijklłmnoópqrsśtuvwxyzżźAĄBCĆDEĘFGHIJKLŁMNOÓPQRSŚTUVWXYZŻŹ";
+}
+
+
+
+
+
+
+
+
+
+
+
+class ContactDataFormatter {
+
+
+
+
+
+
+
+
+    public static String formatPhoneNumber(String phoneNumber) {
+        String formattedNumber = removeNonDigits(phoneNumber);
+        return formattedNumber;
+    }
+
+    public static String formatPhoneNumber(int phoneNumber) {
+        String formattedNumber = Integer.toString(phoneNumber);
+        return formattedNumber;
+    }
+
+
+    
+    private static String removeNonDigits(String number) {
+        String result = "";
+        for (int i = 0; i < number.length(); i++) {
+            if(isDigit(number.charAt(i))) {
+                result += number.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    private static String charRemoveAt(String string, int index) {
+        return string.substring(0, index) + string.substring(index);
+    }
+
+    private static boolean isDigit(char character) {
+        for (int i = 0; i < Constants.DIGITS.length(); i++) {
+            if (character == Constants.DIGITS.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    public static String formatProperName(String string) {
+        String formattedProperName = removeNonLetters(string);
+        formattedProperName = capitaliseString(formattedProperName);
+        return formattedProperName;
+    }
+
+
+    private static String removeNonLetters(String string) {
+        String result = "";
+        for (int i = 0; i < string.length(); i++) {
+            if (isLetter(string.charAt(i))) {
+                result += string.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    private static boolean isLetter(char character) {
+        for (int i = 0; i < Constants.LETTERS.length(); i++) {
+            if (character == Constants.LETTERS.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static String capitaliseString(String string) {
+        return string.substring(0,1).toUpperCase() + string.substring(1).toLowerCase();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
